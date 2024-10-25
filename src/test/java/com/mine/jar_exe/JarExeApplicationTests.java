36d178a8508e3c08717e4bc6ed6dacfd7f65784b
Nandha Kishore S <nandhakishore.s@prodapt.com>
2024-10-25 14:46:52 +0100

    init

---
+++ src/test/java/com/mine/jar_exe/JarExeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mine.jar_exe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarExeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
